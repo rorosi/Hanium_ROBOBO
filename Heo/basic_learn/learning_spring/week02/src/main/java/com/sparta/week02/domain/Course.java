841fe8ddeeca4634c7c0b5bfbbdb5b1933308cf8
HDHpgm <gb78@naver.com>
2021-07-26 23:36:11 +0900

    course controller, dto, repository, service 구조 나눔 연습

--- Heo/basic_learn/learning_spring/week02/src/main/java/com/sparta/week02/domain/Course.java
+++ Heo/basic_learn/learning_spring/week02/src/main/java/com/sparta/week02/domain/Course.java
@@ -1,9 +1,11 @@
 package com.sparta.week02.domain;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@Getter // lombok 으로 설정해줍니다.
 @NoArgsConstructor // 기본생성자를 대신 생성해줍니다.
 @Entity // 테이블임을 나타냅니다.
 public class Course extends Timestamped {
@@ -19,16 +21,9 @@ public class Course extends Timestamped {
     @Column(nullable = false)
     private String tutor;
 
-    public Long getId() {
-        return this.id;
-    }
-
-    public String getTitle() {
-        return this.title;
-    }
-
-    public String getTutor() {
-        return this.tutor;
+    public Course(CourseRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.tutor = requestDto.getTutor();
     }
 
     public Course(String title, String tutor) {
@@ -36,8 +31,8 @@ public class Course extends Timestamped {
         this.tutor = tutor;
     }
 
-    public void update(Course course) {
-        this.title = course.title;
-        this.tutor = course.tutor;
+    public void update(CourseRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.tutor = requestDto.getTutor();
     }
 }
