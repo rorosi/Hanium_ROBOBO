edfc9b303eab8c6764d73234e78eda295b702159
이명현 <dkfmap0123@naver.com>
2021-09-28 15:16:27 +0900

    myung / write 0928

--- Myung/server/arduino_source/myung_ard.ino
+++ Myung/server/arduino_source/myung_ard.ino
@@ -3,14 +3,34 @@
 BLEService Service("19b10000-e8f2-537e-4f6c-d104768a1214");
 
 //BLEUnsignedCharCharacteristic FlexChar("19b10001-e8f2-537e-4f6c-d104768a1214", BLERead|BLEWrite|BLENotify);
-BLEStringCharacteristic FlexChar("19b10001-e8f2-537e-4f6c-d104768a1214", BLERead|BLEWrite, 20);
+BLEStringCharacteristic FlexChar("19b10001-e8f2-537e-4f6c-d104768a1214", BLERead|BLEWrite|BLENotify, 50);
 
 byte read_data = 0;
-String data = "mss daa ccc ddd";
+String data = "";
 String data2 = "";
 int ch = 0;
-byte *temp = new byte[data.length()+1];
 
+String fin1 = "";
+String fin2 = "";
+String fin3 = "";
+String fin4 = "";
+String fin5 = "";
+
+int flex_1 = A1;
+int flex_2 = A2;
+int flex_3 = A3;
+int flex_4 = A4;
+int flex_5 = A5;
+
+int flex_1_val;
+int flex_2_val;
+int flex_3_val;
+int flex_4_val;
+int flex_5_val;
+
+int sum=0;
+int old=0;
+int count = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -21,7 +41,7 @@ void setup() {
     while(1);
   }
 
-  BLE.setLocalName("Ard nano 33 ble (peripheral)");
+  BLE.setLocalName("myung nano 33 ble");
   BLE.setAdvertisedService(Service);
   Service.addCharacteristic(FlexChar);
   BLE.addService(Service);
@@ -43,11 +63,48 @@ void loop(){
     Serial.println(central.address()); //MAC
 
     while(central.connected()){
-        data2 = data + " " + ch;
-        FlexChar.writeValue(data2);
-        Serial.println(data2);
-        ch++;
-        delay(250);
+      flex_1_val = analogRead(flex_1); 
+      flex_1_val = map(flex_1_val, 0, 800, 10, 99);
+      
+      flex_2_val = analogRead(flex_2);
+      flex_2_val = map(flex_2_val, 0, 800, 10, 99);
+      
+      flex_3_val = analogRead(flex_3);
+      flex_3_val = map(flex_3_val, 0, 800, 10, 99);
+      
+      flex_4_val = analogRead(flex_4);
+      flex_4_val = map(flex_4_val, 0, 800, 10, 99);
+      
+      flex_5_val = analogRead(flex_5);
+      flex_5_val = map(flex_5_val, 0, 800, 10, 99);
+
+      fin1 = "1"+(String)flex_1_val;
+      fin2 = "2"+(String)flex_2_val;
+      fin3 = "3"+(String)flex_3_val;
+      fin4 = "4"+(String)flex_4_val;
+      fin5 = "5"+(String)flex_5_val;
+
+      data = fin1+" "+fin2+" "+fin3+" "+fin4+" "+fin5;
+      sum = flex_1_val + flex_2_val + flex_3_val + flex_4_val + flex_5_val;
+
+      if(abs(sum - old)>3){
+        FlexChar.writeValue(data);
+        Serial.println(data);        
+        old = sum;
+        delay(10);
+      }
+      else{
+        old = sum;
+        count = count+1;
+        if(count>15){
+          FlexChar.writeValue(data);
+          Serial.println(data);
+          count = 0;
+        }
+        delay(10);
+      }
+      
+      delay(200);
      }
   }
 
