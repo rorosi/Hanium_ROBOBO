4083ffcea4ae57abd598fa21d344744976f8da30
이명현 <dkfmap0123@naver.com>
2021-10-13 14:30:36 +0900

    Update tae_ard.ino

--- Myung/server/arduino_source/tae_ard.ino
+++ Myung/server/arduino_source/tae_ard.ino
@@ -1,21 +1,28 @@
+#include <Servo.h>
 #include <ArduinoBLE.h>
 
 BLEService Service("19c10000-e8f2-537e-4f6c-d104768a1214");
-
 //BLEUnsignedCharCharacteristic FlexChar("19b10001-e8f2-537e-4f6c-d104768a1214", BLERead|BLEWrite|BLENotify);
-BLEByteCharacteristic FlexChar("19c10001-e8f2-537e-4f6c-d104768a1214", BLERead|BLEWrite|BLENotify);
+BLEStringCharacteristic FlexChar("19c10001-e8f2-537e-4f6c-d104768a1214", BLERead|BLEWrite|BLENotify, 20);
+
+Servo servo;
+
+int servopin = 5;
+
 
 byte read_data = 0;
 String data = "mss daa ccc ddd";
 String data2 = "";
 int ch = 0;
 
-byte h;
-byte hh[20];
+int fin1, fin2, fin3, fin4, fin5;
+int old_sum = 0;
 
 
 void setup() {
   Serial.begin(115200);
+  servo.attach(5);
+  servo.write(150);
   while(!Serial);
 
   if(!BLE.begin()){
@@ -31,7 +38,7 @@ void setup() {
   BLE.advertise();
   Serial.println("nano33 ble peripheral");
   Serial.println(BLE.address());
-
+  
 }
 
 void loop(){
@@ -42,11 +49,49 @@ void loop(){
   if(central){
     Serial.println("conn to central : ");
     Serial.println(central.address()); //MAC
-
+    servo.write(0);
+    delay(1000);
+    servo.write(100);
+    delay(1000);
     while(central.connected()){
-        byte charValue = FlexChar.value();
-        String aa = (String)charValue;
-        Serial.println(charValue);
+        String charValue = FlexChar.value();
+        fin1 = charValue.substring(0,2).toInt();
+        fin2 = charValue.substring(3,5).toInt();
+        fin3 = charValue.substring(6,8).toInt();
+        fin4 = charValue.substring(9,11).toInt();
+        fin5 = charValue.substring(12,14).toInt();
+
+        int sum_data = fin1 + fin2 + fin3 + fin4 + fin5;
+
+        if(abs(sum_data - old_sum)>2){
+          Serial.print("엄지:");
+          Serial.print(fin1);
+          Serial.print(" ");
+    
+          Serial.print("검지:");
+          Serial.print(fin2);
+          Serial.print(" ");
+    
+          Serial.print("중지:");
+          Serial.print(fin3);
+          Serial.print(" ");
+    
+          Serial.print("약지:");
+          Serial.print(fin4);
+          Serial.print(" ");
+    
+          Serial.print("새끼:");
+          Serial.print(fin5);
+          Serial.println(" ");          
+
+          old_sum = sum_data;
+          servo.write(fin2);
+          delay(10);
+        }
+        else{
+          old_sum = sum_data;
+          delay(100);
+        }
     }
   }
   else{
